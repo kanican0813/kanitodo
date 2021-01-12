294db9ad2e65635ba42252cb9add415d5601a5e0
Masa <kanican0813@gmail.com>
2021-01-12 23:43:07 +0900

    repositoryとentityの追加

---
+++ src/main/java/com/example/kanitodo/domain/repository/ITodoItemRepository.java
@@ -0,0 +1,10 @@
+package com.example.kanitodo.domain.repository;
+
+import com.example.kanitodo.infrastructure.entity.TodoItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ITodoItemRepository extends JpaRepository<TodoItem, Long> {
+    public List<TodoItem> findByDoneOrderByTitleAsc(boolean done);
+}
---
+++ src/main/java/com/example/kanitodo/infrastructure/entity/TodoItem.java
@@ -0,0 +1,43 @@
+package com.example.kanitodo.infrastructure.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "todoitems")
+public class TodoItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "todoitems_id_seq")
+    @SequenceGenerator(name = "todoitems_id_seq", sequenceName = "todoitems_id_seq")
+    private Long id;
+    private String title;
+    private Boolean done;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Boolean getDone() {
+        return done;
+    }
+
+    public void setDone(Boolean done) {
+        this.done = done;
+    }
+
+}
